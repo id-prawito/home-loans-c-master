aa39a66e677d36aa1c648e0136a8d357be1d1d63
Prawito <prawito.id@gmail.com>
2021-10-19 18:05:04 +0700

    Fungsi Login dan Lain-lain ++

--- src/components/Dashboard/Petugas/DetailPengajuan.js
+++ src/components/Dashboard/Petugas/DetailPengajuan.js
@@ -1,137 +1,137 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-// import { Select } from "antd";
+import { Form, Button, Select } from "antd";
+import ListKelengkapan from "./ListKelengkapan";
 
 // import TabelDp from "./TabelDp";
 
 export default function DetailPengajuan() {
-    const [user, setUser] = useState([]);
-    const [status, setStatus] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [status, setStatus] = useState("");
     const [isUpdate, setUpdate] = useState(false);
 
-    // const { Option } = Select;
+    const { Option } = Select;
     let { id } = useParams();
 
-    // console.log(useLocation());
-
-    // console.log(">>", id);
     const getUser = async () => {
         let response = await axios.get(`http://localhost:8000/pengajuan/${id}`);
-        setUser(response.data);
-        // console.log(" ini response nya >> ", response.data);
-    };
-
-    const getStatus = async () => {
-        let response = await axios.get(`http://localhost:8000/pengajuan/${id}`);
+        setUsers(response.data);
         setStatus(response.data.status);
-        // console.log(" ini response nya >> ", response.data.status);
     };
 
     useEffect(() => {
         getUser();
     }, [id]);
 
-    useEffect(() => {
-        getStatus();
-    }, [id]);
-
-    // console.log(">>", user);
-    function handleChange(e) {
-        setStatus(e.target.value);
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
+    const handleSubmit = (event) => {
+        event.preventDefault();
         fetch("http://localhost:8000/status", {
             method: "POST",
             body: JSON.stringify({
-                status: "",
+                id: id,
+                id_pengajuan: id,
+                status: status,
             }),
             headers: {
-                "Content-type": "application / json",
+                "Content-type": "application/json; charset=UTF-8",
             },
         })
             .then((response) => response.json())
             .then((json) => {
-                setStatus(true);
+                setUpdate(true);
+                console.log(json);
             });
-    }
+    };
 
     useEffect(() => {
         if (isUpdate) {
-            alert("status sukses di update");
+            alert("Status sukses di update");
             setUpdate(false);
         }
     }, [isUpdate]);
 
-    // const handleAlert = (e) => {
-    //     alert("apakah anda yakin ?");
-    // };
+    function handleChange(e) {
+        setStatus(e);
+    }
+
+    function handleClick(e) {
+        handleSubmit(e);
+    }
 
     return (
-        <div>
+        <>
             <div className="site-layout-background card">
                 <h3 className="title">Data Diri Pengajuan KPR</h3>
                 <div className="data__diri">
-                    <div className="data__content" key={user.id_pengajuan}>
+                    <div className="data__content" key={users.id_pengajuan}>
                         <div className="data__content-pengajuan">
                             <span>Nomor Induk KTP</span>
-                            {user.nik}
+                            {users.nik}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Nama Lengkap</span>
-                            {user.nama_lengkap}
+                            {users.nama_lengkap}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Tempat Tanggal Lahir</span>
-                            {user.tempat_lahir}
+                            {users.tempat_lahir}
                             {", "}
-                            {user.tanggal_lahir}
+                            {users.tanggal_lahir}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Alamat Saat Ini</span>
-                            {user.alamat}
+                            {users.alamat}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Pekerjaan</span>
-                            {user.pekerjaan}
+                            {users.pekerjaan}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Pendapatan Per Bulan</span>
-                            {user.pendapatan_perbulan}
+                            {users.pendapatan_perbulan}
                         </div>
                         <div className="data__content-pengajuan">
-                            <span>Bukti Selfie KTP</span>
-                            <a href="#a">{user.bukti_ktp}</a>
+                            <span>Status</span>
+                            {status}
                         </div>
                         <div className="data__content-pengajuan">
                             <span>Bukti Slip Gaji</span>
-                            <a href="#a">{user.bukti_gaji}</a>
+                            Bukti Slip Gaji
+                            <a href={users.bukti_gaji}>.pdf</a>
                         </div>
-                        <form
-                            onSubmit={(e) => handleSubmit(e)}
-                            className="data__content-pengajuan"
-                        >
-                            <span>Status Verifikasi</span>
-                            {status}
-                            <select
-                                style={{ width: 200, height: 30 }}
-                                onChange={handleChange}
-                            >
-                                <option value="Terverifikasi">
-                                    Terverifikasi
-                                </option>
-                                <option value="Tidak Terverifikasi">
-                                    Tidak Terverifikasi
-                                </option>
-                            </select>
-                            <button type="submit">Submit</button>
-                        </form>
+                        <div className="data__content-pengajuan">
+                            <span>Bukti Selfie KTP</span>
+                            Bukti Selfie KTP
+                            <a href={users.bukti_ktp}>.pdf</a>
+                        </div>
                     </div>
                 </div>
-                <div></div>
+                <div>
+                    <Form>
+                        <Select
+                            defaultValue="Menunggu Verifikasi"
+                            style={{ width: 200, marginTop: 20 }}
+                            onChange={handleChange}
+                        >
+                            <Option value="Terverifikasi">Terverifikasi</Option>
+                            <Option value="Tidak Terverifikasi">
+                                Tidak Terverifikasi
+                            </Option>
+                        </Select>
+                        <Button
+                            type="primary"
+                            onClick={handleClick}
+                            style={{ marginLeft: 10 }}
+                        >
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
-        </div>
+            <div style={{ marginTop: 30 }}>
+                <ListKelengkapan />
+            </div>
+        </>
     );
 }
