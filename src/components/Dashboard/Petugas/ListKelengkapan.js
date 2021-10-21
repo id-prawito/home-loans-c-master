d60b1a24751d2b27369bebd811dec2bd9d535857
abrianto <abriantopringgodigdo@gmail.com>
2021-10-21 21:30:02 +0700

    clear code

--- src/components/Dashboard/Petugas/ListKelengkapan.js
+++ src/components/Dashboard/Petugas/ListKelengkapan.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import baseURL from "../../../configs/axios-variable";
 import { Form, Button, Select } from "antd";
 
 function ListKelengkapan() {
